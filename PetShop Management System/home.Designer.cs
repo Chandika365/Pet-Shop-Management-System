3ab4c7dda720f21b9d8fe85b8cd69b7e7e02c55b
Chandika365 <92178588+Chandika365@users.noreply.github.com>
2022-10-11 14:03:12 +0530

    Add files via upload

---
+++ PetShop Management System/home.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace new_pet_shop
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnhome = new System.Windows.Forms.Button();
+            this.btnproduct = new System.Windows.Forms.Button();
+            this.btnemployee = new System.Windows.Forms.Button();
+            this.btncustomer = new System.Windows.Forms.Button();
+            this.btnbilling = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnlogout = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(28)))), ((int)(((byte)(149)))));
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(243, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1054, 649);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnhome
+            // 
+            this.btnhome.Location = new System.Drawing.Point(41, 157);
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(139, 33);
+            this.btnhome.TabIndex = 1;
+            this.btnhome.Text = "Home";
+            this.btnhome.UseVisualStyleBackColor = true;
+            // 
+            // btnproduct
+            // 
+            this.btnproduct.Location = new System.Drawing.Point(41, 250);
+            this.btnproduct.Name = "btnproduct";
+            this.btnproduct.Size = new System.Drawing.Size(139, 33);
+            this.btnproduct.TabIndex = 2;
+            this.btnproduct.Text = "Product";
+            this.btnproduct.UseVisualStyleBackColor = true;
+            // 
+            // btnemployee
+            // 
+            this.btnemployee.Location = new System.Drawing.Point(41, 327);
+            this.btnemployee.Name = "btnemployee";
+            this.btnemployee.Size = new System.Drawing.Size(139, 33);
+            this.btnemployee.TabIndex = 3;
+            this.btnemployee.Text = "Employee";
+            this.btnemployee.UseVisualStyleBackColor = true;
+            // 
+            // btncustomer
+            // 
+            this.btncustomer.Location = new System.Drawing.Point(41, 416);
+            this.btncustomer.Name = "btncustomer";
+            this.btncustomer.Size = new System.Drawing.Size(139, 33);
+            this.btncustomer.TabIndex = 4;
+            this.btncustomer.Text = "Customer";
+            this.btncustomer.UseVisualStyleBackColor = true;
+            // 
+            // btnbilling
+            // 
+            this.btnbilling.Location = new System.Drawing.Point(41, 506);
+            this.btnbilling.Name = "btnbilling";
+            this.btnbilling.Size = new System.Drawing.Size(139, 33);
+            this.btnbilling.TabIndex = 5;
+            this.btnbilling.Text = "Billing";
+            this.btnbilling.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1301, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(29, 25);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(77)))), ((int)(((byte)(243)))));
+            this.label1.Location = new System.Drawing.Point(38, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Employee Name";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(28)))), ((int)(((byte)(149)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(34, 219);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(407, 121);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(28)))), ((int)(((byte)(149)))));
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(34, 361);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(407, 121);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(28)))), ((int)(((byte)(149)))));
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(34, 502);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(407, 121);
+            this.panel4.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(77)))), ((int)(((byte)(243)))));
+            this.label2.Location = new System.Drawing.Point(3, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dogs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(77)))), ((int)(((byte)(243)))));
+            this.label4.Location = new System.Drawing.Point(3, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cats";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(77)))), ((int)(((byte)(243)))));
+            this.label5.Location = new System.Drawing.Point(3, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Birds";
+            // 
+            // btnlogout
+            // 
+            this.btnlogout.Location = new System.Drawing.Point(41, 597);
+            this.btnlogout.Name = "btnlogout";
+            this.btnlogout.Size = new System.Drawing.Size(139, 33);
+            this.btnlogout.TabIndex = 7;
+            this.btnlogout.Text = "Log out";
+            this.btnlogout.UseVisualStyleBackColor = true;
+            this.btnlogout.Click += new System.EventHandler(this.btnlogout_Click);
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(7)))), ((int)(((byte)(111)))));
+            this.ClientSize = new System.Drawing.Size(1329, 713);
+            this.Controls.Add(this.btnlogout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnbilling);
+            this.Controls.Add(this.btncustomer);
+            this.Controls.Add(this.btnemployee);
+            this.Controls.Add(this.btnproduct);
+            this.Controls.Add(this.btnhome);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "home";
+            this.Text = "home";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnhome;
+        private System.Windows.Forms.Button btnproduct;
+        private System.Windows.Forms.Button btnemployee;
+        private System.Windows.Forms.Button btncustomer;
+        private System.Windows.Forms.Button btnbilling;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlogout;
+    }
+}
